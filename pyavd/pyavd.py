04e0a52b7004b1a26fa3478f806928eb374b636b
SimSams <danya221299@gmail.com>
2023-08-28 09:26:16 -0700

    vm manager

--- pyavd/pyavd.py
+++ pyavd/pyavd.py
@@ -2,9 +2,14 @@ import re
 import os
 import shlex
 import subprocess
+import time
 
-avd_cmd = "avdmanager"
-emulator_cmd = "emulator"
+from loguru import logger
+
+# avd_cmd = "avdmanager"
+# emulator_cmd = "emulator"
+emulator_cmd = r'C:\Users\King\AppData\Local\Android\Sdk\tools\emulator.exe'
+avd_cmd = r'C:\Users\King\AppData\Local\Android\Sdk\cmdline-tools\latest\bin\avdmanager.bat'
 
 
 class Target:
@@ -40,11 +45,12 @@ class Device:
 
 
 class AVD:
-    def __init__(self, name: str, device: Device, path: str, target: str, skin: str, sdcard_size: str, based_on: str, abi: str) -> None:
+    def __init__(self, name: str, device: Device, path: str, target: str, skin: str, sdcard_size: str, based_on: str,
+                 abi: str) -> None:
         self.name = name
         self._device = device
         # Check if path exists
-        if (not os.path.isfile(path)):
+        if not os.path.isfile(path):
             raise Exception("Path does not exist")
         self.path = path
         self.target = target
@@ -52,12 +58,12 @@ class AVD:
         self.sdcard_size = sdcard_size
         self.based_on = based_on
         self.abi = abi
-        self.process = None
+        self.process: subprocess.Popen | None = None
 
     def __init__(self) -> None:
         self._device = None
         self.name = "invalid"
-        self.process = None
+        self.process: subprocess.Popen | None = None
 
     def isEmpty(self) -> bool:
         return self.name == "invalid"
@@ -76,7 +82,7 @@ class AVD:
         device = re.sub("[\(\[].*?[\)\]]", "", device).strip()
         # Find the corrent device to the string
         for d in get_devices():
-            if (d.id_alias == device):
+            if d.id_alias == device:
                 self._device = d
                 return
 
@@ -92,22 +98,22 @@ class AVD:
             raise Exception("Could not find avdmanager")
         return res.stdout and not res.stderr
 
-    def move(self, new_path: str) -> bool:
-        """
-        NOT IMPLEMENTED\n
-        Move the AVD to a new path
-
-        Args:
-            new_path (str): New path to move the AVD to
-
-        Raises:
-            Exception: If avdmanager is not found
-
-        Returns:
-            bool: True if the move was successful, False otherwise
-        """
-        raise NotImplementedError()
-        return False
+    # def move(self, new_path: str) -> bool:
+    #     """
+    #     NOT IMPLEMENTED\n
+    #     Move the AVD to a new path
+    #
+    #     Args:
+    #         new_path (str): New path to move the AVD to
+    #
+    #     Raises:
+    #         Exception: If avdmanager is not found
+    #
+    #     Returns:
+    #         bool: True if the move was successful, False otherwise
+    #     """
+    #     raise NotImplementedError()
+    #     return False
 
     def rename(self, new_name: str) -> bool:
         """
@@ -151,9 +157,12 @@ class AVD:
         Returns:
             subprocess.Popen: The process of the emulator
         """
+        proc = None
+
         cmd_args = [emulator_cmd, "-avd", self.name]
-        if (config):
+        if config:
             cmd_args += shlex.split(config)
+            logger.info(cmd_args)
         try:
             proc = subprocess.Popen(
                 cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -167,10 +176,10 @@ class AVD:
         except subprocess.TimeoutExpired as e:
             if not detach:
                 raise e
-        self.process = proc
+        self.process: subprocess.Popen = proc
         return proc
 
-    def stop(self) -> bool:
+    def stop(self, port) -> bool:
         """
         Stop the AVD
 
@@ -178,10 +187,25 @@ class AVD:
             bool: True if the AVD was stopped, False otherwise
         """
         if not self.process:
+            logger.error("AVD is not found")
             return False
+
         self.process.kill()
         # Wait for zombie process to exit
         self.process.wait()
+
+        name_emu = "emulator-" + port
+        cmd_args = ['adb', '-s', name_emu, 'emu', 'kill']
+        logger.info(f'command for stop emulator {cmd_args}')
+        try:
+            res = subprocess.run(
+                cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            logger.info(res)
+
+        except OSError:
+            raise Exception(f"Could not find {self.name}")
+
         self.process = None
         return True
 
@@ -209,39 +233,39 @@ def get_targets() -> list[Target]:
         current_target = Target()
         for line in res.stdout.decode("utf-8").split("\n"):
             stripped_line = line.strip()
-            if (not stripped_line):
+            if not stripped_line:
                 continue
 
             # If "----------" is seen a new target definition begins
             # So we add the old current target and overwrite it
-            if (stripped_line == "----------"):
-                if (not current_target.isEmpty()):
+            if stripped_line == "----------":
+                if not current_target.isEmpty():
                     targets.append(current_target)
                 current_target = Target()
                 continue
 
             # If there is no : in the line, its not a valid key value pair
-            if (":" not in stripped_line):
+            if ":" not in stripped_line:
                 continue
 
             key, value = stripped_line.split(":")
             key = key.strip().upper()
             value = value.strip()
-            if (key == "id".upper()):
+            if key == "id".upper():
                 id, alias = value.split(" or ")
                 current_target.id = int(id.strip())
                 current_target.id_alias = alias.strip().replace('"', '')
-            elif (key == "Name".upper()):
+            elif key == "Name".upper():
                 current_target.name = value
-            elif (key == "Type".upper()):
+            elif key == "Type".upper():
                 current_target.type = value
-            elif (key == "API Level".upper()):
+            elif key == "API Level".upper():
                 current_target.api_level = int(value)
-            elif (key == "Revision".upper()):
+            elif key == "Revision".upper():
                 current_target.revision = int(value)
 
         # Add the last target
-        if (not current_target.isEmpty()):
+        if not current_target.isEmpty():
             targets.append(current_target)
     return targets
 
@@ -269,36 +293,36 @@ def get_devices() -> list[Device]:
         current_device = Device()
         for line in res.stdout.decode("utf-8").split("\n"):
             stripped_line = line.strip()
-            if (not stripped_line):
+            if not stripped_line:
                 continue
             # If "---------" is seen a new target definition begins
             # So we add the old current target and overwrite it
-            if (stripped_line == "---------"):
-                if (not current_device.isEmpty()):
+            if stripped_line == "---------":
+                if not current_device.isEmpty():
                     devices.append(current_device)
                 current_device = Device()
                 continue
 
             # If there is no : in the line, its not a valid key value pair
-            if (":" not in stripped_line or stripped_line.count(":") > 2):
+            if ":" not in stripped_line or stripped_line.count(":") > 2:
                 continue
             key, value = stripped_line.split(":")
             key = key.strip().upper()
             value = value.strip()
 
-            if (key == "id".upper()):
+            if key == "id".upper():
                 id, alias = value.split(" or ")
                 current_device.id = int(id.strip())
                 current_device.id_alias = alias.strip().replace('"', '')
-            elif (key == "Name".upper()):
+            elif key == "Name".upper():
                 current_device.name = value
-            elif (key == "OEM".upper()):
+            elif key == "OEM".upper():
                 current_device.oem = value
-            elif (key == "Tag".upper()):
+            elif key == "Tag".upper():
                 current_device.tag = value
 
         # Append last device
-        if (not current_device.isEmpty()):
+        if not current_device.isEmpty():
             devices.append(current_device)
 
     return devices
@@ -327,42 +351,42 @@ def get_avds() -> list[AVD]:
         current_avd = AVD()
         for line in res.stdout.decode("utf-8").split("\n"):
             stripped_line = line.strip()
-            if (not stripped_line):
+            if not stripped_line:
                 continue
             # If "---------" is seen a new target definition begins
             # So we add the old current target and overwrite it
-            if (stripped_line == "---------"):
-                if (not current_avd.isEmpty()):
+            if stripped_line == "---------":
+                if not current_avd.isEmpty():
                     avds.append(current_avd)
                 current_avd = AVD()
                 continue
 
             # If there is no : in the line, its not a valid key value pair
-            if (":" not in stripped_line):
+            if ":" not in stripped_line:
                 continue
 
             key, value, *rest = stripped_line.split(":")
             key = key.strip().upper()
             value = value.strip()
-            if (key == "Name".upper()):
+            if key == "Name".upper():
                 current_avd.name = value
-            elif (key == "Device".upper()):
+            elif key == "Device".upper():
                 current_avd.device = value
-            elif (key == "Path".upper()):
+            elif key == "Path".upper():
                 current_avd.path = value
-            elif (key == "Target".upper()):
+            elif key == "Target".upper():
                 current_avd.target = value
-            elif (key == "Skin".upper()):
+            elif key == "Skin".upper():
                 current_avd.skin = value
-            elif (key == "Sdcard".upper()):
+            elif key == "Sdcard".upper():
                 current_avd.sdcard_size = value
-            elif (key == "Based on".upper()):
+            elif key == "Based on".upper():
                 # Based on: Android 12L (Sv2) Tag/ABI: google_apis/x86_64
                 current_avd.based_on = value.replace("Tag/ABI", "").strip()
                 current_avd.abi = rest[0].strip()
 
         # Append last avd
-        if (not current_avd.isEmpty()):
+        if not current_avd.isEmpty():
             avds.append(current_avd)
 
     return avds
@@ -372,9 +396,12 @@ def get_avd_by_name(name: str) -> AVD:
     for avd in get_avds():
         if avd.name == name:
             return avd
+    logger.error(f"AVD {name} not found")
     return None
 
-def create_avd(name: str, package: str, device: Device, force: bool = False, sdcard: str = None, tag: str = None, skin: str = None, abi: str = None, path: str = None) -> AVD:
+
+def create_avd(name: str, package: str, device: Device, force: bool = False, sdcard: str = None, tag: str = None,
+               skin: str = None, abi: str = None, path: str = None) -> AVD:
     """
     Create a new AVD with the given name and package
 
@@ -405,11 +432,14 @@ def create_avd(name: str, package: str, device: Device, force: bool = False, sdc
     cmd_args = [avd_cmd, "create", "avd", "-n",
                 name, "--package", package, "--device", str(device.id)]
     for key, value in inclusion_dict.items():
-        if (value):
+        if value:
             cmd_args.append(key)
             cmd_args.append(str(value))
-    if(force):
-        cmd_args += "--force"
+
+    if force:
+        cmd_args += ["--force"]
+
+    logger.info(cmd_args)
     try:
         subprocess.run(
             cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
